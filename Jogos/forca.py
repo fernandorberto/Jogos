58b5bfe531ba513fed790d89a402aa36bb503338
fernandorberto <fernandorberto@gmail.com>
2021-05-03 20:52:01 -0300

    Inclusão Jogos para gerenciamento de qual jogo escolher + Jogo da forca

---
+++ Jogos/forca.py
@@ -0,0 +1,10 @@
+import random
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
